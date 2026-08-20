63331773a910a8b89e3936336135cd3c726ba060
Mathias Science <mathiasgamern1@gmail.com>
2026-08-20 13:48:13 -0500

    Iniciando con el desarrollo de la pagina

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function actualizarContador() {
     fetch("http://127.0.0.1:8000")
         .then(respuesta => respuesta.json())
         .then(datos => {
-            document.getElementById("texto-servidor").innerText = datos.mensaje + " segundos";
+            document.getElementById("revolucion-industrial").innerText = datos.mensaje + " segundos";
         });
 }
 
